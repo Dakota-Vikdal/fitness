d41719645ece1b94d741b4e2d74b04d1b9107ce4
Dakota Vikdal <dakota.james19@hotmail.com>
2023-04-25 13:34:10 -0400

    signup form, no model yet

--- client/src/components/Auth.js
+++ client/src/components/Auth.js
@@ -1,8 +1,38 @@
+import { useState } from 'react'
 
 export function Signup() {
+    const [form, setForm] = useState({})
+
+    const updateForm = (e) => {
+        setForm (form => {
+            return {
+                ...form,
+                [e.target.name]: e.target.value
+            }
+        })
+    }
+    const createAccount = e => {
+        e.preventDefault()
+        const body = JSON.stringify(form)
+        console.log('ready to post:', body)
+    }
     return (
         <div>
             <h1>Signup</h1>
+            <form onSubmit={createAccount}>
+                <div>
+                    username: <input onChange= {updateForm} name='username' />
+                </div>
+                <div>
+                    password: <input onChange= {updateForm} name='password' />
+                </div>
+                <div>
+                    confirm password: <input onChange= {updateForm} name='confirm' />
+                </div>
+                <div>
+                    <input type='submit' />
+                </div>
+            </form>
         </div>
     )
 }
