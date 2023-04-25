53408e421da4cf92ea1b69c20cc2e1d7fcf27c11
Dakota Vikdal <dakota.james19@hotmail.com>
2023-04-25 13:11:28 -0400

    login/signup components

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,21 @@
 import './stylesheets/App.css';
+import { Switch, Route, Redirect } from 'react-router-dom'
+import { Signup, Login} from './components/Auth.js'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1>We will accomplish great things, mark my words</h1>
-      </header>
+     <div className="App">
+       <header className="App-header">
+       {/* <Header /> */}
+        <Switch>
+            <Route path= '/login' component={Login}/>
+            <Route path= '/Signup' component={Signup}/>
+            {/* <Route path= '/rental' component={Rental}/>
+            <Route path= '/movie' component={Movie}/>
+            <Route path= '/' component={Home}/> */}
+            <Redirect to= '/' />
+        </Switch>
+      </header> 
     </div>
   );
 }
---
+++ client/src/components/Auth.js
@@ -0,0 +1,16 @@
+
+export function Signup() {
+    return (
+        <div>
+            <h1>Signup</h1>
+        </div>
+    )
+}
+
+export function Login() {
+    return(
+        <div>
+            <h1>Login</h1>
+        </div>
+    )
+}
