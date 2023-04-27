d9ebdfb0d147d52d2efdeedbe702b987d5c581ad
Dakota Vikdal <dakota.james19@hotmail.com>
2023-04-27 00:15:18 -0400

    login/signup functionality

--- client/src/App.js
+++ client/src/App.js
@@ -3,17 +3,47 @@ import { Switch, Route } from 'react-router-dom'
 import { Signup, Login} from './components/Auth.js'
 import Home from './components/Home'
 import NavBar from './components/NavBar'
+import {useEffect, useState} from 'react'
 
 function App() {
+  const [user, setUser] = useState(null);
+
+  useEffect(() => {
+      fetch("/check_session").then((response) => {
+          if (response.ok) {
+              response.json().then((user) => setUser(user));
+          }
+      });
+  }, []);
+
+  function handleLogin(user) {
+      setUser(user);
+  }
+
+  function handleLogout() {
+      setUser(null);
+  }
+
+  function handleUpdate(user) {
+      setUser(user)
+  }
+
+
+
   return (
      <div className="App">
        <header className="App-header">
-       {/* <Header /> */}
        <NavBar />
         <Switch>
-            <Route path= '/login' component={Login} />
-            <Route path= '/Signup' component={Signup} />
-            <Route path ='/' component={Home} />
+            <Route path= '/login'>
+              <Login handleLogin = {handleLogin}/>
+            </Route>
+            <Route path= '/signup' >
+              <Signup />
+            </Route>
+            <Route path ='/'>
+              <Home />
+            </Route>
         </Switch>
       </header> 
     </div>
--- client/src/components/Auth.js
+++ client/src/components/Auth.js
@@ -1,56 +1,171 @@
 import { useState } from 'react'
 import NavButton from './NavButton'
+import {useHistory} from 'react-router-dom'
+import {useFormik} from 'formik'
+import * as yup from "yup"
 
-export function Signup() {
-    const [form, setForm] = useState({})
+export function Signup({setUser}) {
 
-    const updateForm = (e) => {
-        setForm (form => {
-            return {
-                ...form,
-                [e.target.name]: e.target.value
-            }
-        })
-    }
-    const createAccount = e => {
-        e.preventDefault()
-        const body = JSON.stringify(form)
-        console.log('ready to post:', body)
-    }
+    const history = useHistory()
+
+    const formSchema = yup.object().shape({
+        username: yup.string().required('Please enter a username'),
+        email: yup.string().email(),
+        password: yup.string().required('Please enter a username')
+    })
+
+    const formik = useFormik({
+        initialValues:{
+            username:"",
+            email:"",
+            password:""
+        },
+        validationSchema: formSchema,
+        onSubmit: (values) => {
+            fetch("/signup", {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify(values),
+            }).then((r) => {
+                if (r.ok) {
+                    r.json().then((user) => setUser(user));
+                }
+            });
+            history.push('/login')
+        },
+    })
+
+
     return (
         <div>
-            <h1>Welcome!</h1>
-            <h3>Sign up below</h3>
-            <form onSubmit={createAccount}>
+            <form onSubmit={formik.handleSubmit}>
+                <h2>Signup!</h2>
                 <div>
-                    username: <input onChange= {updateForm} name='username' />
+                    <label htmlFor="username">Username</label>
+                    <input 
+                    type="text"
+                    name="username"
+                    id="username"
+                    value={formik.values.username}
+                    onChange={formik.handleChange}
+                    />
+                    <p style={{ color: "red" }}> {formik.errors.email}</p>
                 </div>
                 <div>
-                    password: <input onChange= {updateForm} name='password' />
+                    <label htmlFor="password">Password</label>
+                    <input 
+                        type="password"
+                        id="password"
+                        value={formik.values.password}
+                        onChange={formik.handleChange}
+                        autoComplete="current-password" 
+                        />
                 </div>
                 <div>
-                    confirm: <input onChange= {updateForm} name='confirm' />
+                    <label htmlFor="email">Email Address</label>
+                    <input 
+                    type="text"
+                    name="email"
+                    id="email"
+                    value={formik.values.email}
+                    onChange={formik.handleChange}
+                    />
                 </div>
-                <div>
-                    <input type='submit' />
-                </div>
+                <button type="submit">Submit</button>
             </form>
-            <NavButton/>
         </div>
     )
-}
 
-export function Login() {
+}
+   
+
+
+
+
+
+
+
+
+
+
+
+
+
+//     return (
+//         <div>
+//             <h1>Welcome!</h1>
+//             <h3>Sign up below</h3>
+//             <form onSubmit={createAccount}>
+//                 <div>
+//                     username: <input onChange= {updateForm} name='username' />
+//                 </div>
+//                 <div>
+//                     password: <input onChange= {updateForm} name='password' />
+//                 </div>
+//                 <div>
+//                     confirm: <input onChange= {updateForm} name='confirm' />
+//                 </div>
+//                 <div>
+//                     <input type='submit' />
+//                 </div>
+//             </form>
+//             <NavButton/>
+//         </div>
+//     )
+// }
+
+export function Login({handleLogin}) {
+
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
+      
+
+    const history = useHistory()
+
+    function handleSubmit(e) {
+        e.preventDefault();
+        fetch("/login", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+        // body: JSON.stringify({ username}),
+            body: JSON.stringify({ username, password }),
+        }).then((r) => {
+            if (r.ok) {
+                r.json().then((user) => handleLogin(user))
+                history.push('/')
+            }
+            else {
+                return 'uh oh'
+            }
+        });
+    }
+
+
     return(
         <div>
             <h1>Welcome Back!</h1>
             <h3>Ready to get fit? If so, login below.</h3>
-            <form >
+            <form onSubmit={handleSubmit}>
                 <div>
-                    username: <input  name='username' />
+                    <input
+                        type="text"
+                        name="username"
+                        id="username"
+                        value={username}
+                        onChange={(e) => setUsername(e.target.value)} />
                 </div>
                 <div>
-                    password: <input  name='password' />
+                    <input
+                        type="password"
+                        name="password"
+                        id="password"
+                        autoComplete="current-password"
+                        value={password}
+                        onChange={(e) => setPassword(e.target.value)} />
                 </div>
                 <div>
                     <input type='submit' />
@@ -60,3 +175,7 @@ export function Login() {
         </div>
     )
 }
+
+
+
+
